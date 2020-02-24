ab879054bf6aa17e9b5508be8bb04c66b2203b86
Emma Coetzer <emmac@bbd.co.za>
2020-02-24 07:27:55 +0200

    still working on form submission

--- planner/src/main/java/com/grandplan/planner/controllers/LoginController.java
+++ planner/src/main/java/com/grandplan/planner/controllers/LoginController.java
@@ -1,27 +1,28 @@
 package com.grandplan.planner.controllers;
 
 import com.grandplan.planner.models.Login;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
 
-  @GetMapping("/")
-  public String main() {
+  @GetMapping({"/", "/login"})
+  public String login(Model model) {
+    model.addAttribute("user", new Login());
     return "login/login";
   }
 
-  @GetMapping("/login")
-  public String login() {
-    return "login/login";
-  }
-
-  @PostMapping("/validate")
-  public String validate(@ModelAttribute("user") Login user){
-    System.out.println(user);
+  @RequestMapping(value = "/validate", method = RequestMethod.POST)
+  public String validate(@ModelAttribute Login user, BindingResult bindingResult, Model model){
+    model.addAttribute("emailError", "Please enter a valid email");
+    // if(bindingResult.hasErrors()) return "login/login";
     return this.validateLogin(user) ? "home/home" : "login/login";
   }
 
