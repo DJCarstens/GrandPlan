62b611fc6906f468c41c9c9dd9b62df813ec0588
Emma Coetzer <emmac@bbd.co.za>
2020-02-25 07:12:10 +0200

    clear error messages on input focus

--- planner/src/main/java/com/grandplan/planner/controllers/LoginController.java
+++ planner/src/main/java/com/grandplan/planner/controllers/LoginController.java
@@ -2,6 +2,9 @@ package com.grandplan.planner.controllers;
 
 import com.grandplan.planner.models.Login;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,6 +16,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LoginController {
 
+  private Pattern pattern;
+  private Matcher matcher;
+
+  public Model mainModel;
+
+  private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
   @GetMapping({"/", "/login"})
   public String login(Model model) {
     model.addAttribute("user", new Login());
@@ -20,13 +30,35 @@ public class LoginController {
   }
 
   @RequestMapping(value = "/validate", method = RequestMethod.POST)
-  public String validate(@ModelAttribute Login user, BindingResult bindingResult, Model model){
-    model.addAttribute("emailError", "Please enter a valid email");
-    // if(bindingResult.hasErrors()) return "login/login";
-    return this.validateLogin(user) ? "home/home" : "login/login";
+  public String validate(@ModelAttribute("user") Login user, BindingResult bindingResult, Model model){
+    this.mainModel = model;
+    mainModel.addAttribute("login", user);
+    return this.validateLogin(user, mainModel) ? "home/home" : "login/login";
   }
 
-  public boolean validateLogin(Login user){
+  public boolean validateLogin(Login user, Model model){
+    if(user.getEmail() == "" || user.getPassword() == ""){
+      model.addAttribute("emailError", "Please provide your email");
+      model.addAttribute("passwordError", "Please provide your password");
+      return false;
+    }
+
+    if(!validateEmail(user.getEmail())){
+      model.addAttribute("emailError", "Please provide a valid email");
+      return false;
+    }
+
+    return true;
+  }
+
+  public boolean validateEmail(String email){
+    if(email == "") return false;
+    pattern = Pattern.compile(EMAIL_PATTERN);
+    matcher = pattern.matcher(email);
+    return matcher.matches();
+  }
+
+  public boolean findUser(Login user){
     return true;
   }
 
