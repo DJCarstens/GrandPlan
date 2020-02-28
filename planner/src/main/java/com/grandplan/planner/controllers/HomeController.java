3320a47c59468eb1fd00e96a25827fda327e584a
Sam Hillebrand <samah@bbd.co.za>
2020-02-28 21:49:43 +0200

    fixed username input for navbar. Added error page

--- planner/src/main/java/com/grandplan/planner/controllers/HomeController.java
+++ planner/src/main/java/com/grandplan/planner/controllers/HomeController.java
@@ -1,10 +1,8 @@
 package com.grandplan.planner.controllers;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.grandplan.planner.models.EventTemp;
 import com.grandplan.planner.models.User;
 
@@ -24,31 +22,11 @@ public class HomeController {
     user = new User();
     user.setUsername("Testy McTestface");
     model.addAttribute("username", user.getUsername());
-    model.addAttribute("events", mapEvents());
     return "home";
   }
 
-  
-  private String mapEvents() {
-    String jsonEvents = null;
-        try {
-            EventTemp event = new EventTemp();
-            event.setTitle("first event");
-            event.setStart("2020-02-02");
-            events.add(event);
-
-            event = new EventTemp();
-            event.setTitle("second event");
-            event.setStart("2020-03-02");
-            event.setEnd("2020-03-06");
-
-            ObjectMapper mapper = new ObjectMapper();
-            jsonEvents =  mapper.writerWithDefaultPrettyPrinter().writeValueAsString(events);
-        } catch (IOException ioex) {
-            System.out.println(ioex.getMessage());
-        }
-        return jsonEvents;
-
+  @GetMapping("/error")
+  public String error(Model model) {
+    return "error";
   }
-
 }
