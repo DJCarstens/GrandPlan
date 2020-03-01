a38e62ab1031ea16341dfd478f01e14369ff9433
chelseajessiman <53180222+chelseajessiman@users.noreply.github.com>
2020-03-01 21:43:06 +0200

    added @RequiredArgsConstructor and un-bundled imports

--- planner/src/main/java/com/grandplan/server/ServerController.java
+++ planner/src/main/java/com/grandplan/server/ServerController.java
@@ -2,22 +2,24 @@ package com.grandplan.server;
 
 import com.grandplan.server.services.ApiLoginService;
 import com.grandplan.util.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")  //prevents conflict with client mapping (a.k.a. grandPlanController)
 public class ServerController {
 
     @Autowired
     private ApiLoginService apiLoginService;
-
-    public ServerController(ApiLoginService apiLoginService) {
-        this.apiLoginService = apiLoginService;
-    }
 
     @GetMapping("/getUser")
     public User getUser() {
--- planner/src/main/java/com/grandplan/server/services/ApiLoginService.java
+++ planner/src/main/java/com/grandplan/server/services/ApiLoginService.java
@@ -2,19 +2,16 @@ package com.grandplan.server.services;
 
 import com.grandplan.server.repositories.UserRepo;
 import com.grandplan.util.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 @Component
 public class ApiLoginService {
     private final UserRepo userRepo;
-
-    @Autowired
-    public ApiLoginService(UserRepo userRepo) {
-        this.userRepo = userRepo;
-    }
 
     public User validateUserCredentials(User user)
     {
