5b1e947c68f78d20f049ca82cdfa5d54ab07f0d0
Emma Coetzer <emmac@bbd.co.za>
2020-03-03 15:48:02 +0200

    removed @Entity

--- planner/src/main/java/com/grandplan/client/util/Signup.java
+++ planner/src/main/java/com/grandplan/client/util/Signup.java
@@ -3,11 +3,9 @@ package com.grandplan.client.util;
 import com.grandplan.util.User;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import javax.validation.constraints.NotEmpty;
@@ -15,8 +13,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Data
-@Entity
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Signup{
