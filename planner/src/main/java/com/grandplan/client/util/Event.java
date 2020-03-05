b09755ab3f5c5360bbf0eec574522af0cd5f4ca6
Emma Coetzer <emmac@bbd.co.za>
2020-03-05 18:18:34 +0200

    Modal styling changes

---
+++ planner/src/main/java/com/grandplan/client/util/Event.java
@@ -0,0 +1,31 @@
+package com.grandplan.client.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Event{
+   @NotNull(message = "Please provide a title")
+   @NotEmpty(message = "Please provide a title")
+   private String title;
+   private String description;
+   private String[] members;
+
+   @NotNull(message = "Please provide a start date and time")
+   @NotEmpty(message = "Please provide a start date and time")
+   private String start;
+
+   @NotNull(message = "Please provide an end date and time")
+   @NotEmpty(message = "Please provide an end date and time")
+   private String end;
+   private boolean allDay;
+   private String tag;
+   private String color;
+
+}
