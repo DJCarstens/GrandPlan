957d1106d473509fc546365e3a38b3bc0ff1e232
chelseajessiman <53180222+chelseajessiman@users.noreply.github.com>
2020-03-07 09:12:16 +0200

    Response entity returns

--- planner/src/main/java/com/grandplan/server/ServerController.java
+++ planner/src/main/java/com/grandplan/server/ServerController.java
@@ -1,7 +1,7 @@
 package com.grandplan.server;
 
-import com.grandplan.server.services.ApiLoginService;
 import com.grandplan.server.services.ApiEventService;
+import com.grandplan.server.services.ApiLoginService;
 import com.grandplan.util.Event;
 import com.grandplan.util.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Set;
-
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api")  //prevents conflict with client mapping (a.k.a. grandPlanController)
@@ -60,22 +59,22 @@ public class ServerController {
     }
 
     @PostMapping("/getUserEvents")
-    public Set<Event> getUserEvents(String email) {
-        return apiEventService.getUserEvents(email);
+    public ResponseEntity<Set<Event>> getUserEvents(String email) {
+        return ResponseEntity.ok(apiEventService.getUserEvents(email));
     }
 
     @PostMapping("/createEvent")
-    public Event createEvent(Event event) {
-        return apiEventService.createEvent(event);
+    public ResponseEntity<Event> createEvent(Event event) {
+        return ResponseEntity.ok(apiEventService.createEvent(event));
     }
 
     @PostMapping("/deleteEvent")
-    public boolean deleteEvent(Event event) {
-        return apiEventService.deleteEvent(event);
+    public ResponseEntity<Boolean> deleteEvent(Event event) {
+        return ResponseEntity.ok(apiEventService.deleteEvent(event));
     }
 
     @PostMapping("/updateEvent")
-    public Event updateEvent(Event event) {
-        return apiEventService.updateEvent(event);
+    public ResponseEntity<Event> updateEvent(Event event) {
+        return ResponseEntity.ok(apiEventService.updateEvent(event));
     }
 }
