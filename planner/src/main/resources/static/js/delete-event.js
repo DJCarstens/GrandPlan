8769e8780103bc4859bb04396b643652c3d002d7
Emma Coetzer <emmac@bbd.co.za>
2020-03-09 20:53:52 +0200

    removed transfer event option if user is not host

--- planner/src/main/resources/static/js/delete-event.js
+++ planner/src/main/resources/static/js/delete-event.js
@@ -1,15 +1,32 @@
 $(document).ready(function () {
     $(".deleteEvent").click(function() {
-        $(this).find('input').each(function(){
-            let $id = "#" + $(this).val();
-            let $class = "." + $(this).val();
+        let $items = [];
+        $(this).parent().find('input').each(function(){
+            $items.push($(this).val());
+        });
+
+        if($items.length == 1){
+            let $id = "#" + $items[0];
+            let $class = "." + $items[0];
+            $($id).css("opacity", "0.2");
+            $($class).toggle();
+            $($class).css("position", "absolute");
+            $($class).css("top", "45%");            
+            $($class).css("left", "28%");
+            $($class).css("z-index", "99");
+            $($class + " .removeEvent").css("padding", "4%");
+            $($class + " .removeEvent").css("width", "90%");
+        }
+        else{
+            let $id = "#" + $items[1];
+            let $class = "." + $items[1];
             $($id).css("opacity", "0.2");
             $($class).toggle();
             $($class).css("position", "absolute");
             $($class).css("top", "34%");            
             $($class).css("left", "5%");
             $($class).css("z-index", "99");
-        });
+        }
     });
 
     $(".cancel-button").click(function() {
